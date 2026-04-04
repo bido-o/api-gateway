e46d013ad2b755bb1b2dc35d67502b044be6153e
Gabriela1011 <lionte.gabriela@yahoo.com>
2026-04-04 13:46:21 +0300

    refactored jwt validator

--- src/main/java/com/bido/api_gateway/util/JwtValidator.java
+++ src/main/java/com/bido/api_gateway/util/JwtValidator.java
@@ -1,31 +1,39 @@
 package com.bido.api_gateway.util;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 
+import static io.jsonwebtoken.io.Decoders.BASE64;
+
 
 @Component
 public class JwtValidator {
 
-    private final SecretKey secretKey;
+    private final JwtParser jwtParser;
 
-    public JwtValidator(@Value("${jwt.secret}") String secretString) {
-        byte[] keyBytes = Decoders.BASE64.decode(secretString);
-        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+    public JwtValidator(@Value("${jwt.secret}") String secretKeyString) {
+        byte[] keyBytes = BASE64.decode(secretKeyString);
+        SecretKey secretKey = Keys.hmacShaKeyFor(keyBytes);
         //log.info("JwtValidator a fost inițializat și cheia secretă a fost încărcată în memorie.");
+
+        this.jwtParser = Jwts.parser()
+                .verifyWith(secretKey)
+                .build();
     }
 
-    public void validateToken(final String token) {
-        Jwts.parser().verifyWith(this.secretKey).build().parseSignedClaims(token);
+    private Jws<Claims> validateToken(final String token) {
+        return this.jwtParser.parseSignedClaims(token);
     }
 
     public Claims extractAllClaims(final String token) {
-        return Jwts.parser().verifyWith(this.secretKey).build().parseSignedClaims(token).getPayload();
+        return validateToken(token).getPayload();
     }
+
 }
