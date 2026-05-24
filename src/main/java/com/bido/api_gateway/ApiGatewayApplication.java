2d4f755e392e6196d7d8666c263ea823c1850e2f
Gabriela1011 <lionte.gabriela@yahoo.com>
2026-05-24 20:15:30 +0300

    added tracing and logging configuration with Zipkin support

--- src/main/java/com/bido/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/bido/api_gateway/ApiGatewayApplication.java
@@ -1,10 +1,17 @@
 package com.bido.api_gateway;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
+
+	@PostConstruct
+	void enableAutomaticContextPropagation() {
+		Hooks.enableAutomaticContextPropagation();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
